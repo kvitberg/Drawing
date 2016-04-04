bede7c9930b85e76f533f9b4929442eb3cda46c6
Scott <scott.kvitberg@gmail.com>
2016-04-04 14:02:43 +0200

    Added simple drawing functionality for the application

--- Drawing/Drawing/ViewController.swift
+++ Drawing/Drawing/ViewController.swift
@@ -9,13 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var buttonsStackView: UIStackView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var lastPoint = CGPoint.zero
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "appBecameActive", name: UIApplicationDidBecomeActiveNotification, object: nil)
+        
     }
-
+    func appBecameActive() {
+        self.buttonsStackView.hidden = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -25,8 +34,43 @@ class ViewController: UIViewController {
         
         //Hide the navigation bar in root ViewController
         self.navigationController?.navigationBarHidden = true
+        self.buttonsStackView.hidden = false
     }
-
-
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        print("Began touch")
+        if let touch = touches.first{
+            let point = touch.locationInView(self.imageView)
+            self.lastPoint = point
+        }
+        self.buttonsStackView.hidden = true
+    }
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first{
+            let point = touch.locationInView(self.imageView)
+            print(point)
+            
+            
+            UIGraphicsBeginImageContext(self.imageView.frame.size)
+            let context = UIGraphicsGetCurrentContext()
+            
+            self.imageView.image?.drawInRect(CGRect(x:0, y:0, width: self.imageView.frame.size.width, height: self.imageView.frame.size.height))
+            
+            CGContextMoveToPoint(context, self.lastPoint.x , self.lastPoint.y)
+            CGContextAddLineToPoint(context, point.x, point.y)
+            
+            CGContextStrokePath(context)
+            
+            self.imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+            
+            UIGraphicsEndImageContext()
+            self.lastPoint = point
+        }
+    }
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        print("Ended touch")
+        self.buttonsStackView.hidden = false
+    }
+    
+    
 }
 
