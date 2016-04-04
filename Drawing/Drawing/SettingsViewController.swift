0b1c14e9580dc79f665a907539a9f1f85dc23983
Scott <scott.kvitberg@gmail.com>
2016-04-04 13:18:22 +0200

    Added UI for the application

---
+++ Drawing/Drawing/SettingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.swift
+//  Drawing
+//
+//  Created by Scott  on 04/04/16.
+//  Copyright © 2016 ScottKvitberg. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //Show the navigation bar in Settings
+        self.navigationController?.navigationBarHidden = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
